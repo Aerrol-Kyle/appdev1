b4843cec1b45600d5496c6825b6bbf38eb5d3db1
Aerrol-Kyle <santoskyle128@gmail.com>
2024-10-11 15:35:31 +0800

    I'm done completing the section on Responding to events

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,17 +1,13 @@
-const products = [
-  {title:'Cabbage', id: 1},
-  {title: 'Garlic', id: 2},
-  {title: 'Apple', id: 3},
-];
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
 
-export default function ShoppingList() {
-  const listItems = products.map(product =>
-    <li key={product.id}>
-      {product.title}
-    </li>
-  );
-  
   return (
-    <ul>{listItems}</ul>
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 }
+
+export default MyButton
