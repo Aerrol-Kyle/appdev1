48efedee10feb12aec8a9e7ed27b0be75b2e8f6e
Aerrol-Kyle <santoskyle128@gmail.com>
2024-11-16 14:24:53 +0800

    Done with Student Profile

--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import Counter from './Counter.jsx'
 import ToggleButton from './ToggleButton.jsx'
 import FormInput from './FormInput.jsx'
-import FetchPosts from './FetchingData.jsx'
+import FetchPosts from './FetchingPosts.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')).render(
     <ToggleButton />
     <FormInput />
     <FetchPosts />
+    <App />
   </StrictMode>,
 )
