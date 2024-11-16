ea9b0bdbb0c42ed841edf45d8f4b686a94c7adb5
Aerrol-Kyle <santoskyle128@gmail.com>
2024-11-16 13:53:10 +0800

    Done with Form Input

--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Counter from './Counter.jsx'
 import ToggleButton from './ToggleButton.jsx'
+import FormInput from './FormInput.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Counter />
     <ToggleButton />
+    <FormInput />
   </StrictMode>,
 )
