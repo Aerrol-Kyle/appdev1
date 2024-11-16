cfc80af24e9f42e99678dab5926729234c9469ab
Aerrol-Kyle <santoskyle128@gmail.com>
2024-11-16 14:02:34 +0800

    Done with Fetching Posts

--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import Counter from './Counter.jsx'
 import ToggleButton from './ToggleButton.jsx'
 import FormInput from './FormInput.jsx'
+import FetchPosts from './FetchingData.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Counter />
     <ToggleButton />
     <FormInput />
+    <FetchPosts />
   </StrictMode>,
 )
